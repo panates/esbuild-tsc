af3265f3a597a6c54d6ccabbb9ce9a01aec377f6
Eray Hanoğlu <e.hanoglu@panates.com>
2024-08-19 17:11:36 +0300

    Fixed major bug

--- src/index.ts
+++ src/index.ts
@@ -4,80 +4,61 @@ import { inspect } from 'node:util';
 import type { Plugin, PluginBuild } from 'esbuild';
 import typescript from 'typescript';
 
-function esbuildPluginTsc(
-  options?: EsbuildPluginTsc.Options,
-): EsbuildPluginTsc {
-  return new EsbuildPluginTsc(options);
+function esbuildTsc(options?: esbuildTsc.Options): Plugin {
+  const tsconfigPath = options?.tsconfigPath || 'tsconfig.json';
+  return {
+    name: 'tsc',
+    setup(build: PluginBuild) {
+      const parsedTsConfig = esbuildTsc.parseTsConfig(
+        tsconfigPath,
+        process.cwd(),
+      );
+      if (parsedTsConfig.options?.sourceMap) {
+        parsedTsConfig.options.sourceMap = false;
+        parsedTsConfig.options.inlineSources = true;
+        parsedTsConfig.options.inlineSourceMap = true;
+      }
+      const filterFn =
+        typeof options?.filter === 'function' ? options?.filter : undefined;
+
+      build.onLoad(
+        {
+          filter:
+            typeof options?.filter === 'object' ? options.filter : /\.tsx?$/,
+        },
+        async args => {
+          if (filterFn) {
+            if (!filterFn(args.path, parsedTsConfig)) return;
+          } else if (!parsedTsConfig?.options?.emitDecoratorMetadata) {
+            return;
+          }
+
+          const fileContent = fs.readFileSync(args.path, 'utf8');
+          const program = typescript.transpileModule(fileContent, {
+            compilerOptions: parsedTsConfig.options,
+            fileName: path.basename(args.path),
+          });
+          return { contents: program.outputText };
+        },
+      );
+    },
+  };
 }
 
-export default esbuildPluginTsc;
+export default esbuildTsc;
 
 /**
- * @namespace EsbuildPluginTsc
+ * @namespace esbuildTsc
  */
-export namespace EsbuildPluginTsc {
+namespace esbuildTsc {
   export interface Options {
     tsconfigPath?: string;
     filter?:
       | RegExp
       | ((filename: string, tsconfig: typescript.ParsedCommandLine) => boolean);
   }
-}
 
-/**
- * @class EsbuildPluginTsc
- */
-export class EsbuildPluginTsc implements Plugin {
-  name: string = 'tsc';
-  options: Required<EsbuildPluginTsc.Options>;
-
-  constructor(options?: EsbuildPluginTsc.Options) {
-    this.options = {
-      tsconfigPath: options?.tsconfigPath || 'tsconfig.json',
-      filter: options?.filter || /\.tsx?$/,
-    };
-  }
-
-  setup(build: PluginBuild) {
-    const parsedTsConfig = this._parseTsConfig(
-      this.options.tsconfigPath,
-      process.cwd(),
-    );
-    if (parsedTsConfig.options?.sourceMap) {
-      parsedTsConfig.options.sourceMap = false;
-      parsedTsConfig.options.inlineSources = true;
-      parsedTsConfig.options.inlineSourceMap = true;
-    }
-    const filterFn =
-      typeof this.options.filter === 'function'
-        ? this.options.filter
-        : undefined;
-
-    build.onLoad(
-      {
-        filter:
-          typeof this.options.filter === 'object'
-            ? this.options.filter
-            : /\.tsx?$/,
-      },
-      async args => {
-        if (filterFn) {
-          if (!filterFn(args.path, parsedTsConfig)) return;
-        } else if (!parsedTsConfig?.options?.emitDecoratorMetadata) {
-          return;
-        }
-
-        const fileContent = fs.readFileSync(args.path, 'utf8');
-        const program = typescript.transpileModule(fileContent, {
-          compilerOptions: parsedTsConfig.options,
-          fileName: path.basename(args.path),
-        });
-        return { contents: program.outputText };
-      },
-    );
-  }
-
-  protected _parseTsConfig(
+  export function parseTsConfig(
     tsconfig: string,
     cwd = process.cwd(),
   ): typescript.ParsedCommandLine {
@@ -108,12 +89,12 @@ export class EsbuildPluginTsc implements Plugin {
       baseDir,
     );
     if (parsedTsConfig.errors.length) {
-      this._printDiagnostics(parsedTsConfig.errors);
+      printDiagnostics(parsedTsConfig.errors);
     }
     return parsedTsConfig;
   }
 
-  protected _printDiagnostics(...args: any[]) {
+  export function printDiagnostics(...args: any[]) {
     // eslint-disable-next-line no-console
     console.log(inspect(args, false, 10, true));
   }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,20 +1,10 @@
-import esbuildPluginTsc from '../src/index.js';
+import esbuildTsc from '../src/index.js';
 
 describe(`plugin tests`, () => {
   it(`should create the plugin`, () => {
-    const plugin = esbuildPluginTsc();
+    const plugin = esbuildTsc();
     expect(plugin).toBeDefined();
     expect(typeof plugin.name).toBe('string');
     expect(plugin.setup).toBeInstanceOf(Function);
-  });
-
-  it(`should set tsconfigPath option if not set`, () => {
-    const plugin = esbuildPluginTsc();
-    expect(plugin.options.tsconfigPath).toStrictEqual('tsconfig.json');
-  });
-
-  it(`should set filter option if not set`, () => {
-    const plugin = esbuildPluginTsc();
-    expect(plugin.options.filter).toStrictEqual(/\.tsx?$/);
   });
 });
